9cea0a1d72d1d1e56f72b489c9a9451e6d9b327c
hhendrycks <hmhendrycks@gmail.com>
2022-02-07 16:00:40 -0500

    Added js and python solutions for building arrays.

--- easy_level_problems/easy_level.py
+++ easy_level_problems/easy_level.py
@@ -93,3 +93,25 @@ class Solution:
             else:
                 num /= 2
         return steps
+
+'''
+    * 1920
+    * Given a zero-based permutation nums (0-indexed), 
+    * build an array ans of the same length where ans[i] = nums[nums[i]] 
+    * for each 0 <= i < nums.length and return it.
+
+    * A zero-based permutation nums is an array of distinct integers 
+    * from 0 to nums.length - 1 (inclusive).
+
+    * Input: nums = [5,0,1,2,3,4]
+    * Output: [4,5,0,1,2,3]
+    * Explanation: The array ans is built as follows:
+    * ans = [nums[nums[0]], nums[nums[1]], nums[nums[2]], 
+    * nums[nums[3]], nums[nums[4]], nums[nums[5]]]
+    * = [nums[5], nums[0], nums[1], nums[2], nums[3], nums[4]]
+    * = [4,5,0,1,2,3]
+'''
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        # List comprenhension builds our array for us
+        return [nums[nums[index]] for index in range(len(nums))]
