371b2446043b110ccb961a221d85bd5c7f13652d
hhendrycks <hmhendrycks@gmail.com>
2022-02-17 13:05:19 -0500

    JS and Python solution by iterating through each pattern and checking for appearance in word.

--- easy_level_problems/easy_level.py
+++ easy_level_problems/easy_level.py
@@ -354,3 +354,27 @@ class Solution:
                 seenEdges[node2] = 1
             centerNode = node1 if seenEdges[node1] > seenEdges[node2] else node2
         return centerNode
+
+'''
+    * 1967
+    * Given an array of strings patterns and a string word, 
+    * return the number of strings in patterns that exist as a substring in word.
+    * A substring is a contiguous sequence of characters within a string.
+
+    * Example
+    * Input: patterns = ["a","abc","bc","d"], word = "abc"
+    * Output: 3
+    * Explanation:
+    * - "a" appears as a substring in "abc".
+    * - "abc" appears as a substring in "abc".
+    * - "bc" appears as a substring in "abc".
+    * - "d" does not appear as a substring in "abc".
+    * 3 of the strings in patterns appear as a substring in word.
+'''
+class Solution:
+    def numOfStrings(self, patterns: List[str], word: str) -> int:
+        stringCount = 0
+        for string in patterns:
+            if word.find(string) != -1:
+                stringCount += 1
+        return stringCount
