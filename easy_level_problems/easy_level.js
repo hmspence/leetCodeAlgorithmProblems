558f0524cd42aef3ea0ccbbd9a4714305380c1bb
hhendrycks <hmhendrycks@gmail.com>
2022-02-17 13:30:48 -0500

    JS and Python solution by iterating through start time, using iterating index to access and compare start and end time to query time for each student.

--- easy_level_problems/easy_level.js
+++ easy_level_problems/easy_level.js
@@ -404,3 +404,30 @@ var numOfStrings = function (patterns, word) {
   });
   return stringCount;
 };
+
+/**
+  * 1450
+  * Given two integer arrays startTime and endTime and given an integer queryTime.
+  * The ith student started doing their homework at the time startTime[i]
+  * and finished it at time endTime[i].
+  * Return the number of students doing their homework at time queryTime.
+  * More formally, return the number of students where queryTime lays
+  * in the interval [startTime[i], endTime[i]] inclusive.
+
+  * Example
+  * Input: startTime = [1,2,3], endTime = [3,2,7], queryTime = 4
+  * Output: 1
+  * Explanation: We have 3 students where:
+  * The first student started doing homework at time 1 and finished at time 3 and wasn't doing anything at time 4.
+  * The second student started doing homework at time 2 and finished at time 2 and also wasn't doing anything at time 4.
+  * The third student started doing homework at time 3 and finished at time 7 and was the only student doing homework at time 4.
+ */
+var busyStudent = function (startTime, endTime, queryTime) {
+  let busyStudents = 0;
+  startTime.forEach((_, i) => {
+    if (startTime[i] <= queryTime && endTime[i] >= queryTime) {
+      busyStudents = busyStudents + 1;
+    }
+  });
+  return busyStudents;
+};
