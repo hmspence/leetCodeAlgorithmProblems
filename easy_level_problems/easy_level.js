ff3a991700f800aff2ef782f407c863f776406eb
hhendrycks <hmhendrycks@gmail.com>
2022-02-18 13:43:06 -0500

    Added JS and Python solution for smallestEqual by iterating through array until the index mod 10 is equal to the current number. Then break out of the loop.

--- easy_level_problems/easy_level.js
+++ easy_level_problems/easy_level.js
@@ -431,3 +431,30 @@ var busyStudent = function (startTime, endTime, queryTime) {
   });
   return busyStudents;
 };
+
+/**
+  * 2057
+  * Given a 0-indexed integer array nums,
+  * return the smallest index i of nums such that i mod 10 == nums[i],
+  * or -1 if such index does not exist.
+  * x mod y denotes the remainder when x is divided by y.
+
+  * Example
+  * Input: nums = [0,1,2]
+  * Output: 0
+  * Explanation:
+  * i=0: 0 mod 10 = 0 == nums[0].
+  * i=1: 1 mod 10 = 1 == nums[1].
+  * i=2: 2 mod 10 = 2 == nums[2].
+  * All indices have i mod 10 == nums[i], so we return the smallest index 0.
+ */
+var smallestEqual = function (nums) {
+  let smallestIndex = -1;
+  for (let i = 0; i < nums.length; i++) {
+    if (i % 10 === nums[i]) {
+      smallestIndex = i;
+      break;
+    }
+  }
+  return smallestIndex;
+};
