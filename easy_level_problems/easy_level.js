3d4cfde690ea3fe47cf883895672c6314781c2d1
hhendrycks <hmhendrycks@gmail.com>
2022-02-23 14:33:57 -0500

    Python and JS solutions for FizzBuzz, using mod operator and conditional statements to return array.

--- easy_level_problems/easy_level.js
+++ easy_level_problems/easy_level.js
@@ -549,3 +549,34 @@ var stringMatching = function (words) {
   }
   return [...subStrings];
 };
+
+/**
+  * 412
+  * Given an integer n, return a string array answer (1-indexed) where:
+  * answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+  * answer[i] == "Fizz" if i is divisible by 3.
+  * answer[i] == "Buzz" if i is divisible by 5.
+  * answer[i] == i (as a string) if none of the above conditions are true.
+
+  * Example 1
+  * Input: n = 3
+  * Output: ["1","2","Fizz"]
+  * Example 2
+  * Input: n = 5
+  * Output: ["1","2","Fizz","4","Buzz"]
+ */
+var fizzBuzz = function (n) {
+  const fizzBuzzArr = [];
+  for (let i = 1; i < n + 1; i++) {
+    if (i % 15 === 0) {
+      fizzBuzzArr.push("FizzBuzz");
+    } else if (i % 5 === 0) {
+      fizzBuzzArr.push("Buzz");
+    } else if (i % 3 === 0) {
+      fizzBuzzArr.push("Fizz");
+    } else {
+      fizzBuzzArr.push(i + "");
+    }
+  }
+  return fizzBuzzArr;
+};
--- easy_level_problems/easy_level.py
+++ easy_level_problems/easy_level.py
@@ -516,3 +516,33 @@ class Solution:
                     if word1 in word2:
                         subStrings.add(word1)
         return list(subStrings)
+
+'''
+    * 412
+    * Given an integer n, return a string array answer (1-indexed) where:
+    * answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+    * answer[i] == "Fizz" if i is divisible by 3.
+    * answer[i] == "Buzz" if i is divisible by 5.
+    * answer[i] == i (as a string) if none of the above conditions are true.
+
+    * Example 1
+    * Input: n = 3
+    * Output: ["1","2","Fizz"]
+    * Example 2
+    * Input: n = 5
+    * Output: ["1","2","Fizz","4","Buzz"]
+'''
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        fizzBuzzArr = []
+        for i in range(1, n + 1):
+            if i % 15 == 0:
+                fizzBuzzArr.append("FizzBuzz")
+            elif i % 5 == 0:
+                fizzBuzzArr.append("Buzz")
+            elif i % 3 == 0:
+                fizzBuzzArr.append("Fizz")
+            else:
+                fizzBuzzArr.append(str(i))
+        return fizzBuzzArr
+        
